d6741eab0bbb87ed3be6f5a421b13bb2b99176db
JoannaPichon <pichon.joanna@yahoo.fr>
2021-03-26 16:24:32 +0100

    commentaires

---
+++ .history/src/DataFixtures/AppFixtures_20210326162100.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use App\Entity\Booking;
+use App\Entity\Comment;
+use App\Entity\User;
+use App\Entity\Image;
+use App\Entity\Role;
+use Cocur\Slugify\Slugify;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        //creation dun admin
+        $adminUser = new User();
+        $adminUser   -> setFirstName('Joanna')
+                    -> setLastName('Pichon')
+                    -> setEmail('mail@admin.fr')
+                    -> setPicture('https://www.w3schools.com/howto/img_avatar.png')
+                    -> setPassword($this->passwordEncoder->encodePassword($adminUser,'heyheyhey'))
+                    -> setIntroduction('Superbe appartement en plein coeur du centre ville de Paris')
+                    -> setDescription('Réservez un séjour pleines de surprises dans ce merveilleux appartement, avec vue splendide sur la station de RER nommée "Châtelet", d\'une taille de 13 mètres carrés.<br/>
+            Grâce à son spacieux salon dune taille de 5 mètres carrés, vous pourrez profiter d\'un espace de vie "cocooning" où tout est à portée de main. <br/>
+                Appréciez aussi le doux bruit des voisins qui ont la bienveillance de vous faire profiter du son haut de gamme de leur télévision Leader Price, en particulier durant les matchs de foot.<br/>
+                Bénéficiant d\'une magnifique baignoire tout-en-un d\'une longueur de 95 centimètres, observez la praticité que vous apporte la possibilité de se laver, faire ses besoins et se brosser les dents, tout ça, au même endroit.<br/>
+                Avec l\'option "Bed & Breakfast", disponible pour un surcoût de seulement 25€ par personne, vous pourrez bénéficier d\'un petit déjeuner, déposé avec amour sur le paillasson, comprenant une baguette de pain et un croissant. <br/>
+                Sur la table basse du salon, qui fait aussi office de table à repasser, vous trouverez à votre disposition un guide pour découvrir les meilleurs coins du quartier, comme par exemple le bois de Boulogne, où les meilleurs guides de Paris sauront vous conseiller pour votre balade.')
+                    -> setSlug('joanna-pichon')
+                    -> addUserRole($adminRole);
+        $manager->persist($adminUser);
+
+        //boucle user 1-5
+        for ($k=0; $k <= 5; $k++) { 
+            $user = new User();
+            $user   -> setFirstName('Jean'.$k)
+                    -> setLastName('Dupond'.$k)
+                    -> setEmail('jean'.$k.'.dupond'.$k.'@mail.fr')
+                    -> setPicture('https://www.w3schools.com/howto/img_avatar.png')
+                    -> setPassword($this->passwordEncoder->encodePassword($user,'password'))
+                    -> setIntroduction('Superbe appartement en plein coeur du centre ville de Paris')
+                    -> setDescription('Réservez un séjour pleines de surprises dans ce merveilleux appartement, avec vue splendide sur la station de RER nommée "Châtelet", d\'une taille de 13 mètres carrés.<br/>
+            Grâce à son spacieux salon dune taille de 5 mètres carrés, vous pourrez profiter d\'un espace de vie "cocooning" où tout est à portée de main. <br/>
+                Appréciez aussi le doux bruit des voisins qui ont la bienveillance de vous faire profiter du son haut de gamme de leur télévision Leader Price, en particulier durant les matchs de foot.<br/>
+                Bénéficiant d\'une magnifique baignoire tout-en-un d\'une longueur de 95 centimètres, observez la praticité que vous apporte la possibilité de se laver, faire ses besoins et se brosser les dents, tout ça, au même endroit.<br/>
+                Avec l\'option "Bed & Breakfast", disponible pour un surcoût de seulement 25€ par personne, vous pourrez bénéficier d\'un petit déjeuner, déposé avec amour sur le paillasson, comprenant une baguette de pain et un croissant. <br/>
+                Sur la table basse du salon, qui fait aussi office de table à repasser, vous trouverez à votre disposition un guide pour découvrir les meilleurs coins du quartier, comme par exemple le bois de Boulogne, où les meilleurs guides de Paris sauront vous conseiller pour votre balade.')
+                    -> setSlug('Jean'.$k.'-'.'Dupond'.$k);
+
+
+
+                    
+            $manager -> persist($user);
+
+            $slugify = new Slugify();
+            $title = "titré de l'ànnonçe n°!";
+            $slug = $slugify -> slugify($title);
+            
+            //boucle ad 1-5
+            for ($i=1; $i <= mt_rand(1,5) ; $i++) { 
+
+                $ad = new Ad();
+                $ad -> setTitle('Titre de l\'annonce n°' . $i)
+                    -> setSlug($slug . $i)
+                    -> setCoverImage('https://picsum.photos/seed/300/200')
+                    -> setIntroduction('<strong>Introduction</strong> de l\'annonce ' . $i)
+                    -> setContent('<p>Contenu de l\'annonce ' . $i)
+                    -> setPrice(mt_rand(30,150))
+                    -> setRooms(mt_rand(1,5))
+                    -> setAuthor($user);
+        
+                    //ajout images à une annonce
+                for ($j=0; $j < mt_rand(2,5); $j++) { 
+                    $image = new Image();
+                    $image  -> setUrl('https://picsum.photos/seed/200/200')
+                            -> setCaption('Légende de l\'image '. $j)
+                            -> setAd($ad);
+                    $manager -> persist($image);
+                }
+        
+                $manager -> persist($ad);
+                $manager -> flush();
+        
+                $slug2 = $ad -> getSlug().'_'.$ad -> getId();
+                $ad -> setSlug($slug2);
+        
+                $manager -> persist($ad);
+                
+                //mettre toutes les dates à minuit 00:00:00 !!
+                //boucle booking 0-5
+                for ($j=0; $j < mt_rand(0,5) ; $j++) { 
+                    $booking = new Booking();
+                    $startDate = new \DateTime("+ 5 days");
+                    $startDate->setTime(0,0,0,0);
+                    $endDate = new \DateTime("+ 12 days");
+                    $endDate->setTime(0,0,0,0);
+                    $booking->setAd($ad)
+                            ->setBooker($user)
+                            ->setStartDate($startDate)
+                            ->setEndDate($endDate)
+                            ->setCreatedAt(new \DateTime())
+                            ->setAmount($ad->getPrice()*7)
+                            ->setComment("N'oubliez pas le lit pour bébé svp");
+                    $manager->persist($booking);
+
+                    if (mt_rand(0,1)) {
+                        $comment = new Comment();
+                        $comment->setCreatedAt(new \DateTime())
+                                ->setRating(mt_rand(0,5))
+                                ->setContent("Super séjour $j")
+                                ->setAuthor($user)
+                                ->setAd($ad);
+                        $manager->persist($comment);
+                    }
+                }
+            }        
+        }
+        $manager->flush();
+    } 
+}
---
+++ .history/src/Entity/Ad_20210326160315.php
@@ -0,0 +1,371 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks; 
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=AdRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Ad
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 100,
+     *      minMessage = "Le titre doit faire au minimum {{ limit }} caractères",
+     *      maxMessage = "Le titre ne doit faire au maximum que {{ limit }} caractères",
+     *      allowEmptyString = false
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $introduction;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Url
+     * @Assert\Regex(
+     *     pattern="#\.(jpg|jpeg|png)$#",
+     *     match=true,
+     *     message="Votre URL doit se finir par .jpg, .jpeg ou .png."
+     * )
+     */
+    private $coverImage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rooms;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="ad", orphanRemoval=true)
+     * @Assert\Valid
+     */
+    private $images;
+
+    /**
+     * @ORM\OneToMany(targetEntity=ImageUpload::class, mappedBy="ad", orphanRemoval=true)
+     */
+    private $imageUploads;
+    
+    //champs pour suppr images qui contiendra les id des imges a suppr
+
+    public $idArray;
+    
+
+    /**
+     * @Assert\All({
+     *  @Assert\File(
+     *      maxSize = "4096k",
+     *      maxSizeMessage = "Veuillez entrer une image inférieure à 1mo",
+     *      mimeTypes = {"image/jpeg",
+     *                  "image/png"},
+     *      mimeTypesMessage ="Entrer un jpg ou un jpeg"
+     * )
+     * })
+     * 
+     */
+    
+    public $file;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="ads")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Booking::class, mappedBy="ad")
+     */
+    private $bookings;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="ad", orphanRemoval=true)
+     */
+    private $comments; //concerne images telechargees afin de pouvoir mettre des contraintes de validation
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+        $this->imageUploads = new ArrayCollection();
+        $this->bookings = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(string $introduction): self
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->coverImage;
+    }
+
+    public function setCoverImage(string $coverImage): self
+    {
+        $this->coverImage = $coverImage;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getAd() === $this) {
+                $image->setAd(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ImageUpload[]
+     */
+    public function getImageUploads(): Collection
+    {
+        return $this->imageUploads;
+    }
+
+    public function addImageUpload(ImageUpload $imageUpload): self
+    {
+        if (!$this->imageUploads->contains($imageUpload)) {
+            $this->imageUploads[] = $imageUpload;
+            $imageUpload->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImageUpload(ImageUpload $imageUpload): self
+    {
+        if ($this->imageUploads->removeElement($imageUpload)) {
+            // set the owning side to null (unless already changed)
+            if ($imageUpload->getAd() === $this) {
+                $imageUpload->setAd(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * @ORM\PreRemove
+     */
+    public function deleteUploadsFiles() {
+        $images = $this -> getImageUploads();
+        foreach ($images as $image) {
+           unlink($_SERVER['DOCUMENT_ROOT'] .  $image -> getUrl());
+        }
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Booking[]
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->bookings->removeElement($booking)) {
+            // set the owning side to null (unless already changed)
+            if ($booking->getAd() === $this) {
+                $booking->setAd(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getAd() === $this) {
+                $comment->setAd(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getAvgRatings() {
+        $avg;
+        $comments = $this->getComments();
+        foreach ($comments as $comment) {
+            $rate = $comment->getRating();
+            $avg += $rate;
+            
+        }
+
+
+    }
+}
