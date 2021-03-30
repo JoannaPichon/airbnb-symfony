92e299899dcedc09473940d2d4790e526d24cc5c
JoannaPichon <pichon.joanna@yahoo.fr>
2021-03-30 11:34:09 +0200

    pagination dahsboard ad

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -10,15 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminAdController extends AbstractController
 {
     /**
-     * @Route("/admin/ads", name="admin_ads_index")
+     * @Route("/admin/ads/{page}", name="admin_ads_index")
      */
-    public function index(AdRepository $repo): Response
+    public function index(AdRepository $repo, $page=1): Response
     {
-        $ads = $repo->findAll();
+        $limit = 10;
+        $offset = $page * $limit - $limit ;
+        $pages = ceil(count($repo->findAll())/$limit);
 
+        //$repo = $this   -> getDoctrine()-> getRepository(Ad::class);
+        $ads = $repo -> findBy(array(), ['id' => 'ASC'], $limit, $offset);
 
         return $this->render('admin/ad/index.html.twig', [
-            'ads' => $ads,
-            ]);
+            'page' => $page,
+            'pages' => $pages,
+            'ads' => $ads
+        ]);
     }
 }
